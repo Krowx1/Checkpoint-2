782976395ceea23aea10d8435d54174f46a24e74
Krow <katie.scantland@stu.glasgow.kyschools.us>
2024-08-21 09:24:20 -0500

    Initial Commit

---
+++ checkpoint 2.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+let album = ["Make believe", "The Blue Album", "The Green Album"]
+album.count
+
+let regiment = ["Scouts", "Garrison", "MP"]
+regiment.count
+
+let shifter = ["Attack", "jaw", "war hammer", "armored", "colossal", "female", "cart", "founding", "beast"]
+shifter.count
